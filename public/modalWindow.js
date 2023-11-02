6171a11c0aaf65963142ce34b4a1cb3077da9df4
Eduard2001 <zalyaev2001@list.ru>
2023-11-02 06:01:32 +0300

    Modal window

---
+++ public/modalWindow.js
@@ -0,0 +1,11 @@
+const showPopup = document.querySelector('.show-popup')
+const popupContainer = document.querySelector('.popup-container')
+const closeBtn = document.querySelector('.close-btn')
+
+showPopup.onclick = () => {
+    popupContainer.classList.add('active')
+}
+
+closeBtn.onclick = () => {
+    popupContainer.classList.remove('active')
+}
