6edebcf96cc390cf0e01b4e7f8a18b3051de291f
Eduard0606 <zalyaev200@list.ru>
2023-11-02 19:40:39 +0300

    path fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,16 @@
+import { resolve } from 'path'
+
 export default {
-  base: "/diplom/"
+  base: "/diplom/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        card: resolve(__dirname, 'cardForm.html'),
+        clock: resolve(__dirname, 'clock.html'),
+        modal: resolve(__dirname, 'modalWindow.html'),
+        projects: resolve(__dirname, 'projects.html'),
+      }
+    }
+  }
 }
